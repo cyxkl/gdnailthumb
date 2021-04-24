29634aad54b3ab9450596928764bdd6821c10a5d
cyxkl <72583322+cyxkl@users.noreply.github.com>
2021-04-23 21:01:27 -0700

    Official Release

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-﻿const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
-
+﻿
 //select all req elements
 //selecting all required elements
 const filterItem = document.querySelector(".items");
